8c1246bb00bf540296261f020e4e94ff50eee55d
snehanjali-g <gudimetlasnehanjali@gmail.com>
2025-08-22 20:55:36 +0530

    Add files via upload

---
+++ source code/parametrss.m
@@ -0,0 +1,19 @@
+%% parameters pv side
+%% six phase pmsm
+prasi=2e3;
+time=[0 0.5];
+torquel=[4.5 4.5];
+wrated=2000;
+np=5;
+R=0.3;
+ld=5.56e-3;
+lq=7e-3;
+oaxis=0.125e-3;
+rphas=0.3;
+fluxsta=0.042;
+ja=0.02;
+%% battery side
+ratbat=50;
+vba=144;
+imaxchar=25;
+imaxdisc=25;
